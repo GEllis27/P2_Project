6d4e78a1d78ee7d4b9ff7506a96a069f8d43598f
jacobdozier23 <jacob.dozier@students.fhu.edu>
2026-04-02 12:43:45 -0500

    inital push

--- main.py
+++ main.py
@@ -9,3 +9,26 @@ pygame.display.set_caption("Pong!")
 CLOCK = pygame.time.Clock()
 
     
+class Paddle:
+    def __init__(self, Y, width, height, speed):
+        self.Y = Y
+        self.width = width
+        self.height = height
+        self.speed = speed
+    
+    def move_up(self):
+        self.velocity = -self.speed
+    
+    def move_down(self):
+        self.velocity = self.speed
+    
+    def stop(self):
+        self.velocity = 0
+
+    def update(self, screen_height):
+        self.rect.y += self.velocity
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+    
